2c480b4ff9e0f6ffe854458b02445b343e8315ab
Spandan Saxena <spandansaxena@gmail.com>
2020-03-25 03:14:09 +0530

    Added PiP Support
    The File Consists of a Java Script File Throttled into a Comment inside webView Settings

--- app/src/main/java/com/aaxena/covid19tracker/Landing.java
+++ app/src/main/java/com/aaxena/covid19tracker/Landing.java
@@ -1,20 +1,57 @@
 package com.aaxena.covid19tracker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PictureInPictureParams;
 import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Rational;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class Landing extends AppCompatActivity {
     private Button graphview;
-
+    private Button enter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Kindly Turn On Data Services for First Use", Snackbar.LENGTH_LONG);
+        snackbar.show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        //PiP Mode
+
+        enter = findViewById(R.id.enter_button);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view)
+            {
+                Display d = getWindowManager()
+                        .getDefaultDisplay();
+                Point p = new Point();
+                d.getSize(p);
+                int width = p.x;
+                int height = p.y;
+
+                Rational ratio
+                        = new Rational(width, height);
+                PictureInPictureParams.Builder
+                        pip_Builder
+                        = new PictureInPictureParams
+                        .Builder();
+                pip_Builder.setAspectRatio(ratio).build();
+                enterPictureInPictureMode(pip_Builder.build());
+            }
+        });
+
         graphview = findViewById(R.id.graph);
+        //End of PiP
         graphview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/aaxena/covid19tracker/StatisticView.java
+++ app/src/main/java/com/aaxena/covid19tracker/StatisticView.java
@@ -8,16 +8,28 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class StatisticView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staistic_view);
+        //Toast to Be Patient
+
+        Toast.makeText(StatisticView.this,
+                "Be Patient for First Load", Toast.LENGTH_LONG).show();
+
+       //WebView Implementation
 
         WebView webView = new WebView(this);
         setContentView(webView);
+        //webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://google.org/crisisresponse/covid19-map");
         webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.scrollTo(0, 690);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+
     }
 }
